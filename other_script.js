f4adbaa31d950f2b18d67cf43ba010d19f0c1b7e
Paul <paul_grant@med.unc.edu>
2023-02-09 15:05:19 -0500

    On branch service_test Your branch is up to date with 'origin/service_test'.
    Changes to be committed:
    	modified:   chip-490-crud.html
    	new file:   clock.js
    	new file:   crud_v2.html
    	modified:   other_script.js

--- other_script.js
+++ other_script.js
@@ -1,15 +1,100 @@
-function startTime() {
-  const today = new Date();
-  let h = today.getHours();
-  let m = today.getMinutes();
-  let s = today.getSeconds();
-  m = checkTime(m);
-  s = checkTime(s);
-  document.getElementById('txt').innerHTML =  h + ":" + m + ":" + s;
-  setTimeout(startTime, 1000);
+const FHIRURL = "https://r3.smarthealthit.org";
+
+const client = FHIR.client(FHIRURL);
+// console.clear();
+
+function handleData(data) {
+  for (var d in data.entry) {
+    $("#practitioners").append($("<tr>")
+      .html("<td>"
+      +data.entry[d].resource.id+"</td><td>"
+      +data.entry[d].resource.name[0].family+"</td><td>"
+      +data.entry[d].resource.name[0].given[0]+"</td><td>"
+      +data.entry[d].resource.birthDate+"</td><td>"
+      +"</td>"
+        ));
+  }
 }
 
-function checkTime(i) {
-  if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
-  return i;
+client.request("Practitioner?").then(handleData).catch(console.error);
+
+function showPractitionersByName(lastname) {
+	client.request("Practitioner?name="+lastname).then(handleData).catch(console.error);	
+}
+//showPatientsByName("Smith");
+function showPractitioners() {
+	console.clear();
+	showPractitionersByName("Who");
+}
+
+function addPractitioner(practitioner) {
+  $.ajax({
+    type: "POST",
+    url: FHIRURL+'/Practitioner',
+    headers: {
+    },
+    data: JSON.stringify(practitioner),
+    success: function (data) {
+      alert("Added a Doctor! Should see it in results soon!");
+    },
+    error: function(er) {
+      console.log(er);
+    },
+    datatype: 'json',
+    contentType: 'application/json+fhir;charset=utf-8'
+  });
+}
+
+function addDoctor() {
+  addPractitioner({
+    "resourceType": "Practitioner",
+    "name": {
+      "given": ["Doctor"],
+      "family": "Who"
+    },
+    "birthDate": "1963-03-23"
+  });
+}
+
+function updatePractioner(practitioner) {
+  $.ajax({
+    type: "PUT",
+    url: FHIRURL+'/Practitioner/'+practitioner.id,
+    headers: {
+    },
+    data: JSON.stringify(practitioner),
+    success: function (data) {
+      alert("Updated a Doctor! Should see it in results soon!");
+    },
+    error: function(er) {
+      console.log(er);
+    },
+    datatype: 'json',
+    contentType: 'application/json+fhir;charset=utf-8'
+  });
+}
+
+function updateDoctor() {
+  client.request("Practitioner?name=Who").then(function(data) {
+    var doc = data.entry[0].resource;
+    doc.name[0].given = ["The Doctor"];
+    updatePractioner(doc);
+  }).catch(console.error);
+}
+
+function deletePractitioner(practitioner) {
+  $.ajax({
+    type: "DELETE",
+    url: FHIRURL+'/Practitioner/'+practitioner.id,
+    headers: {
+    },
+    success: function (data) {
+      alert("Deleted a Doctor! Should see it in results soon!");
+    },
+    error: function(er) {
+      console.log(er);
+    },
+    datatype: 'json',
+    contentType: 'application/json+fhir;charset=utf-8'
+  });
 }
